2ff42d9864a6a85dddfd6b8c2f66e4f25d14cc7a
Sleska <shumakov_99@internet.ru>
2023-05-21 19:19:34 +0300

    ProductsView и тестирование

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from products.views import products, ProductsView
+from products.views import ProductsView
 
 app_name = 'products'
 
 urlpatterns = [
     path('', ProductsView.as_view(), name='products'),
-    path('page/<int:page>/', ProductsView.as_view(), name='test'),
     path('category/<int:category>/page/<int:page>/', ProductsView.as_view(), name='filtered_products'),
 ]
--- products/views.py
+++ products/views.py
@@ -32,24 +32,3 @@ class ProductsView(ListView):
         if self.kwargs.get('category'):
             queryset = queryset.filter(category=self.kwargs['category'])
         return queryset
-
-
-def products(request, category=None, number_of_page=1):
-    categories = ProductCategory.objects.all()
-    products_list = Product.objects.all()
-
-    if category:
-        products_list = products_list.filter(category_id=category)
-
-    paginator = Paginator(products_list, 3)
-    products_list = paginator.page(number_of_page)
-
-    context = {
-        'title': 'Store - Каталог',
-        'products_list': products_list,
-        'categories': categories,
-        'number_of_page': 1,
-        'paginator': paginator,
-        'category_id': category or 0,
-    }
-    return render(request, template_name='products/products.html', context=context)
